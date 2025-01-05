b3a02b33809bf1234e24db1115c24b84b5a13c93
SmileDoge97 <Ing_JosFco_JarMtz_SD@outlook.es>
2025-01-05 13:53:25 -0600

    le modifiqué el api de compilación a android 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.aristidevs.convertirdorunidadesandroid"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.aristidevs.convertirdorunidadesandroid"
--- app/src/main/java/com/aristidevs/convertirdorunidadesandroid/MainActivity.kt
+++ app/src/main/java/com/aristidevs/convertirdorunidadesandroid/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.aristidevs.convertirdorunidadesandroid
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.Wallpapers
 import com.aristidevs.convertirdorunidadesandroid.ui.theme.ConvertirdorUnidadesAndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,7 +40,11 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true,
+    device = "spec:parent=pixel_9", name = "Pixel 9",
+    uiMode = Configuration.UI_MODE_NIGHT_NO or Configuration.UI_MODE_TYPE_NORMAL,
+    wallpaper = Wallpapers.NONE
+)
 @Composable
 fun GreetingPreview() {
     ConvertirdorUnidadesAndroidTheme {
