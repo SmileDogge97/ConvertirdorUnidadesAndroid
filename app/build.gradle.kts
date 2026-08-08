a7526426b223f67a2fda96d5bbdaa6994e11007c
SmileDogge97 <Ing_JosFco_JarMtz_SD@outlook.es>
2026-08-07 19:46:52 -0600

    Actualicé el identificador de la aplicación

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.jfsolutions.convertirdorunidadesandroid"
+        applicationId = "com.jfjm_solutions.convertirdorunidadesandroid"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
