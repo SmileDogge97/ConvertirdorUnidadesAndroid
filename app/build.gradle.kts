dc279173eb007222f344551d0d823c1447c0e863
SmileDogge97 <Ing_JosFco_JarMtz_SD@outlook.es>
2026-08-17 17:59:33 -0600

    Se actualizó el targetSdk a 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     defaultConfig {
         applicationId = "com.jfjm_solutions.convertirdorunidadesandroid"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
